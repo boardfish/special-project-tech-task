1436d5392f9cbb484c36531a9145fd24494a8215
Simon Fish <si@mon.fish>
2020-02-28 17:07:19 +0000

    Validate all inputs as positive

--- getbirthday.py
+++ getbirthday.py
@@ -1,3 +1,12 @@
 class GetBirthDay:
-    def foo():
-        return 12
+    def __init__(self, day, month, year):
+        date_sections = [day, month, year]
+        if len(list(filter(lambda x: x >= 0, date_sections))) < 3:
+            raise ValueError 
+        self.day = day
+        self.month = month
+        self.year = year
+    
+
+    def getBirthDay(self):
+        return 6
--- tests.py
+++ tests.py
@@ -6,11 +6,23 @@ class TestGetBirthDay(unittest.TestCase):
     def setUp(self):
         pass
 
-    def inputs_within_upper_limits(self, upperLimit):
-        self.assertLess(GetBirthDay.foo(), upperLimit)
+    def output_under_upper_limit(self, gbd_instance, upperLimit):
+        self.assertLess(gbd_instance.getBirthDay(), upperLimit)
     
-    def test_foo_within_upper_limits(self):
-        self.inputs_within_upper_limits(15)
+    def output_over_lower_limit(self, gbd_instance, lowerLimit):
+        self.assertGreater(gbd_instance.getBirthDay(), lowerLimit)
+    
+    def test_errors_if_year_outside_limits(self):
+        self.assertRaises(ValueError, lambda: GetBirthDay(-1, 1, 1900))
+        self.assertRaises(ValueError, lambda: GetBirthDay(1, -1, 1900))
+        self.assertRaises(ValueError, lambda: GetBirthDay(1, -1, -1))
+        self.assertRaises(ValueError, lambda: GetBirthDay(32, 1, 1900))
+        self.assertRaises(ValueError, lambda: GetBirthDay(1, 13, 1900))
+    
+    def test_outputs_within_limits(self):
+        gbd = GetBirthDay(1, 1, 1900)
+        self.output_over_lower_limit(gbd, -1)
+        self.output_under_upper_limit(gbd, 7)
 
 if __name__ == '__main__':
     unittest.main()
